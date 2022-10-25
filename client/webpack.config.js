439b813947b1e2a6c5d0ba8fa54ae6d7354bdb51
Johnny <johnathan.mnn@gmail.com>
2022-10-25 11:19:58 -0500

    Added css loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,8 +23,11 @@ module.exports = () => {
 
     module: {
       rules: [
-        
-      ],
+        {
+        test: /\.css$/i,
+        use: ['style-loader','css-loader']
+      }
+    ],
     },
   };
 };
