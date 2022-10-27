a4a9f7a75d1f1d240f71476003550416111f3259
Johnny <johnathan.mnn@gmail.com>
2022-10-27 10:21:55 -0500

    Added gettign and putting to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,31 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// The following logic puts the inputed text to the indexedDB database 
+export const putDb = async (content) => {
+  console.log('Putting to database...');
+  // Make database connection
+  const jateDb = await openDB('jate', 1);
+  // Make transaction
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: id, jate: content });
+  const result = await request;
+  console.log('Data saved.', result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Gets everything from the database
+export const getDb = async () => {
+  console.log('Getting all data...');
+  // Make database connection
+  const jateDb = await openDB('jate', 1);
+  // Make transaction
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
